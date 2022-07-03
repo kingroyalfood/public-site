bb65a74c9601bf81facf9de62fc1ab553e09e0b4
Allan Hart <allan.w.hart@gmail.com>
2022-07-03 14:03:10 +0800

    Introduced content helpers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,41 +6,43 @@ import Typography from "@mui/material/Typography";
 
 // import styles from './index.module.scss';
 
+import {
+  ContentSection,
+  ContentSectionHeading,
+} from "components/helpers";
 import { staticMedia } from "util/shortcuts";
 
 function IndexPage() {
   return (
     <>
-      <Grid container component="section" spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h2" color="primary" gutterBottom>
-            Welcome!
-          </Typography>
+      <ContentSection>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <ContentSectionHeading text="Welcome!" />
 
-          <Typography fontSize={20}>
-            We are King Royal Food, a package and food delivery service
-            brand that is committed to cooking serving healthy culinary
-            delights, providing clients with delicious shacks without
-            any hassle.
-          </Typography>
+            <Typography fontSize={20}>
+              We are King Royal Food, a package and food delivery service
+              brand that is committed to cooking serving healthy culinary
+              delights, providing clients with delicious shacks without
+              any hassle.
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Box
+              alt="Friends enjoying resoles together"
+              component="img"
+              src={staticMedia('/product-1.jpg')}
+              width="904"
+              height="625"
+              sx={{ width: '100%' }}
+            />
+          </Grid>
         </Grid>
+      </ContentSection>
 
-        <Grid item xs={12} md={6}>
-          <Box
-            alt="Friends enjoying resoles together"
-            component="img"
-            src={staticMedia('/product-1.jpg')}
-            width="904"
-            height="625"
-            sx={{ width: '100%' }}
-          />
-        </Grid>
-      </Grid>
-
-      <Box component="section">
-        <Typography id="menu" variant="h2" color="primary" gutterBottom>
-          Menu
-        </Typography>
+      <ContentSection>
+        <ContentSectionHeading text="Menu" />
         <Box
           alt="Plate of resoles"
           component="img"
@@ -49,16 +51,14 @@ function IndexPage() {
           height="625"
           sx={{ width: '100%' }}
         />
-      </Box>
+      </ContentSection>
 
-      <Box component="section">
-        <Typography id="order" variant="h2" color="primary" gutterBottom>
-          Order
-        </Typography>
+      <ContentSection>
+        <ContentSectionHeading text="Order" />
         <Typography>
           Order Form
         </Typography>
-      </Box>
+      </ContentSection>
     </>
   );
 }
